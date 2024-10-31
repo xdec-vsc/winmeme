34e0ea1ea6868307b7e96541f462c954ac237606
xdec <145052411+xdec-vsc@users.noreply.github.com>
2024-10-31 10:40:43 +0300

    ödev50 and ödev51 upload

---
+++ ödev50 - hata gösterme örnek/ödev50/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev50
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double giriş, kare;
+            try
+            {
+                giriş = Convert.ToDouble(textBox1.Text);
+                kare = giriş * giriş;
+                MessageBox.Show(kare.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("hatalı giriş");
+            }
+            finally
+            {
+                //"normal" kod
+            }
+        }
+    }
+}
---
+++ ödev51 - iono/ödev51/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev51
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int i;
+            if (radioButton1.Checked == true)
+            {
+                for (i = 1; i <= 10; i=i+2)
+                {
+                    if (i >= 7)
+                    {
+                        continue;
+                    }
+                    listBox1.Items.Add(i);
+                }
+            }
+            else if (radioButton2.Checked == true)
+            {
+                for (i = 1; i <= 10; i = i + 2)
+                {
+                    listBox1.Items.Add(i);
+                    if (i >= 7)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
